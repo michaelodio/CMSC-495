1e215122befa76b9bc39c7e0472b78c237563658
michaelodio <michaelfodio@gmail.com>
2018-09-29 23:01:08 -0400

    added timer to close login window after 2 minutes

--- src/Group7Bank.java
+++ src/Group7Bank.java
@@ -5,5 +5,15 @@ public class Group7Bank {
 
        Input frame = new Input();
        frame.setVisible(true);
+
+       new java.util.Timer().schedule(
+   	        new java.util.TimerTask() {
+   	            @Override
+   	            public void run() {
+   	            	frame.setVisible(false);
+   	            }
+   	        },
+   	        120000
+   	);
     }
 }
