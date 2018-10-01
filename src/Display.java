a51fc5da07c9309b5352a5753ebb232c980a62b3
michaelodio <michaelfodio@gmail.com>
2018-10-01 15:13:30 -0400

    allow user to deposit funds

--- src/Display.java
+++ src/Display.java
@@ -1,6 +1,5 @@
 package proto2;
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,6 +8,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Vector;
 
 import javax.swing.ButtonGroup;
@@ -36,14 +38,14 @@ public class Display extends JDialog {
 	private JTable accountTable;
 	private JScrollPane jScrollPane1;
 	Account custAct;
-	ResultSet rs =null;
+
 
 
 	public Display() {
 
 
 		setTitle("G7 Bank");
-		setBounds(100, 100, 357, 509);
+		setBounds(100, 100, 404, 509);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
@@ -69,6 +71,7 @@ public class Display extends JDialog {
 		JRadioButton rdbtnChecking = new JRadioButton("Checking");
 		rdbtnChecking.setBounds(151, 7, 109, 23);
 		contentPanel.add(rdbtnChecking);
+		rdbtnChecking.setSelected(true);
 
 		JRadioButton rdbtnSavings = new JRadioButton("Savings");
 		rdbtnSavings.setBounds(151, 26, 109, 23);
@@ -88,152 +91,227 @@ public class Display extends JDialog {
         contentPanel.add(transactionAmount);
         transactionAmount.setColumns(10);
 
-        JLabel lblChkAccountBalance = new JLabel("Checking Account Balance:");
-        lblChkAccountBalance.setBounds(10, 169, 168, 14);
-        contentPanel.add(lblChkAccountBalance);
-
         JSeparator separator = new JSeparator();
-        separator.setBounds(10, 125, 321, 2);
+        separator.setBounds(10, 123, 368, 4);
         contentPanel.add(separator);
 
-        JLabel lblNewLabel = new JLabel("Account Details");
-        lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
-        lblNewLabel.setBounds(100, 138, 149, 14);
-        contentPanel.add(lblNewLabel);
-        {
-        	JButton okButton = new JButton("OK");
-        	okButton.setBounds(226, 89, 66, 23);
-        	contentPanel.add(okButton);
-        	okButton.addActionListener(new ActionListener() {
-        		@Override
-        		public void actionPerformed(ActionEvent e) {
-        			StringBuilder warnings = new StringBuilder();
+        JPanel detailsPannel = new JPanel();
+        detailsPannel.setBounds(10, 138, 368, 321);
+        contentPanel.add(detailsPannel);
+        detailsPannel.setLayout(null);
+        detailsPannel.setVisible(false);
 
-        			if(transactionAmount.getText().isEmpty()) {
-        				warnings.append("Transaction amount must not be empty\n");
-        			} else {
-        				try {
-        					transAmount = Integer.parseInt(transactionAmount.getText());
-        				}catch(NumberFormatException ex) {
-        					warnings.append("Balance must be a number");
-        				}
+        JLabel label = new JLabel("Checking Account Balance:");
+        label.setBounds(20, 52, 129, 14);
+        detailsPannel.add(label);
 
-        			}
-        			if (rdbtnChecking.isSelected()) {
-        		    	accountType = "Checking";
+        JLabel lblCheckingAccountDetails = new JLabel("Checking Account Details");
+        lblCheckingAccountDetails.setBounds(89, 11, 221, 20);
+        lblCheckingAccountDetails.setFont(new Font("Tahoma", Font.BOLD, 16));
+        detailsPannel.add(lblCheckingAccountDetails);
 
-        		    }
-        		    else if (rdbtnSavings.isSelected()) {
-        		    	accountType = "Savings";
-        		    }
-        			if(warnings.length()>0) {
-        				JOptionPane.showMessageDialog(null, warnings.toString());
-        			}
-        			else {
+        JLabel label_2 = new JLabel("Checking Interest Earned:");
+        label_2.setBounds(20, 74, 126, 14);
+        detailsPannel.add(label_2);
+
+        JLabel label_3 = new JLabel("");
+        label_3.setBounds(184, 41, 0, 0);
+        detailsPannel.add(label_3);
+
+        JButton button_1 = new JButton("Transaction History");
+        button_1.setBounds(121, 246, 125, 23);
+        detailsPannel.add(button_1);
+
+        JLabel label_4 = new JLabel("Savings Account Balance:");
+        label_4.setBounds(20, 188, 123, 14);
+        detailsPannel.add(label_4);
+
+        JLabel label_5 = new JLabel("Savings Interest Earned:");
+        label_5.setBounds(20, 213, 120, 14);
+        detailsPannel.add(label_5);
+
+        JSeparator separator_1 = new JSeparator();
+        separator_1.setBounds(310, 64, 0, 2);
+        detailsPannel.add(separator_1);
+
+        JButton button_2 = new JButton("Transaction History");
+        button_2.setBounds(121, 105, 125, 23);
+        detailsPannel.add(button_2);
+
+        JLabel label_7 = new JLabel("");
+        label_7.setBounds(157, 88, 0, 0);
+        detailsPannel.add(label_7);
+
+        JLabel label_8 = new JLabel("New label");
+        label_8.setBounds(301, 52, 46, 14);
+        detailsPannel.add(label_8);
+
+        JLabel lblSavingsAccountDetails = new JLabel("Savings Account Details");
+        lblSavingsAccountDetails.setBounds(78, 139, 195, 20);
+        lblSavingsAccountDetails.setFont(new Font("Tahoma", Font.BOLD, 16));
+        detailsPannel.add(lblSavingsAccountDetails);
+
+        JLabel label_9 = new JLabel("New label");
+        label_9.setBounds(301, 74, 46, 14);
+        detailsPannel.add(label_9);
+
+        JLabel label_10 = new JLabel("New label");
+        label_10.setBounds(312, 188, 46, 14);
+        detailsPannel.add(label_10);
+
+        JLabel label_1 = new JLabel("New label");
+        label_1.setBounds(310, 213, 46, 14);
+        detailsPannel.add(label_1);
+
+        JButton button = new JButton("OK");
+        button.setBounds(247, 89, 47, 23);
+        contentPanel.add(button);
+        button.addActionListener(new ActionListener() {
+        	@Override
+			public void actionPerformed(ActionEvent e) {
 
 
-        				Input.createNewAccount(Input.username,accountType, transAmount);
-        				transactionAmount.setText(null);      				
+        		StringBuilder warnings = new StringBuilder();
 
-        			}
+    			if(transactionAmount.getText().isEmpty()) {
+    				warnings.append("Transaction amount must not be empty\n");
+    			} else {
+    				try {
+    					transAmount = Integer.parseInt(transactionAmount.getText());
+    				}catch(NumberFormatException ex) {
+    					warnings.append("Balance must be a number");
+    				}
 
-        		}
-        	});
-        	okButton.setActionCommand("OK");
-        	getRootPane().setDefaultButton(okButton);
-        }
+    			}
+    			if (rdbtnChecking.isSelected()) {
+    		    	accountType = "Checking";
 
+    		    }
+    		    else if (rdbtnSavings.isSelected()) {
+    		    	accountType = "Savings";
+    		    }
+    			if(warnings.length()>0) {
+    				JOptionPane.showMessageDialog(null, warnings.toString());
+    			}
+    			else {
+    				if(accountType != "Savings") {
 
+    				Input.createNewAccount(Input.username,accountType, transAmount);
+    				Transaction.depositFunds(transAmount,getAccountNumChecking(Input.username));
+    				}else {
+    					Input.createNewAccount(Input.username,accountType, transAmount);
+        				Transaction.depositFunds(transAmount,getAccountNumSavings(Input.username));
+    				}
 
-        JLabel lblInterestEarned = new JLabel("Checking Interest Earned:");
-        lblInterestEarned.setBounds(10, 182, 135, 14);
-        contentPanel.add(lblInterestEarned);
+    				}
+    				transactionAmount.setText(null);
 
-        JLabel lblInterestEarned1 = new JLabel("");
-        lblInterestEarned1.setBounds(208, 193, 52, 14);
-        contentPanel.add(lblInterestEarned1);
+        	}
+        });
+        button.setActionCommand("OK");
 
-        JButton btnNewButton = new JButton("Transaction History");
+        JButton btnNewButton = new JButton("View Account Details");
         btnNewButton.addActionListener(new ActionListener() {
         	@Override
 			public void actionPerformed(ActionEvent e) {
-        		displayTransactionHistory();
+        		detailsPannel.setVisible(true);
         	}
         });
-        btnNewButton.setBounds(100, 213, 149, 23);
+        btnNewButton.setBounds(67, 91, 135, 23);
         contentPanel.add(btnNewButton);
-
-        JLabel lblSavingsAccountBalance = new JLabel("Savings Account Balance:");
-        lblSavingsAccountBalance.setBounds(10, 314, 168, 14);
-        contentPanel.add(lblSavingsAccountBalance);
-
-        JLabel lblSavingsInterestEarned = new JLabel("Savings Interest Earned:");
-        lblSavingsInterestEarned.setBounds(10, 329, 135, 14);
-        contentPanel.add(lblSavingsInterestEarned);
-
-        JSeparator separator_1 = new JSeparator();
-        separator_1.setBounds(10, 257, 321, 2);
-        contentPanel.add(separator_1);
-
-        JLabel label = new JLabel("Account Details");
-        label.setFont(new Font("Tahoma", Font.BOLD, 16));
-        label.setBounds(100, 264, 149, 14);
-        contentPanel.add(label);
-
-        JButton button = new JButton("Transaction History");
-        button.setBounds(100, 364, 149, 23);
-        contentPanel.add(button);
-
-        JLabel lblchkBalance = new JLabel("New label");
-        lblchkBalance.setBounds(285, 169, 46, 14);
-        contentPanel.add(lblchkBalance);
-
-        JLabel lblChkInterestEarned = new JLabel("New label");
-        lblChkInterestEarned.setBounds(285, 182, 46, 14);
-        contentPanel.add(lblChkInterestEarned);
-
-        JLabel lblSavActBal = new JLabel("New label");
-        lblSavActBal.setBounds(285, 314, 46, 14);
-        contentPanel.add(lblSavActBal);
-
-        JLabel lblSavInt = new JLabel("New label");
-        lblSavInt.setBounds(285, 329, 46, 14);
-        contentPanel.add(lblSavInt);
-
-
-
-
-
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-		}
 	}
 
 
+       @SuppressWarnings("unchecked")
+	public int getAccountNumChecking(String user) {
+    	   ArrayList num = new ArrayList();
+
+    	   Database database = new Database();
+           Connection dbConn = database.getConnection();
+           Statement stmt = null;
+
+           int largest;
+
+           try {
+               String sql = "SELECT ID FROM ACCOUNTS WHERE USERNAME = ? AND ACCOUNTTYPE = ?";
+               PreparedStatement pst = dbConn.prepareStatement(sql);
+               pst.setString(1, user);
+               pst.setString(2,"Checking");
+               ResultSet rs = pst.executeQuery();
+
+               while (rs.next()){
+               	num.add(rs.getInt("ID"));
+               }
+
+
+           } catch (SQLException e) {
+               System.out.println("------------------TableInsert-----------------");
+               System.out.println("Cannot insert into table: " + e);
+               System.out.println("--------------------------------------------------------");
+           } finally {
+               if (stmt != null) {
+                   try {
+   					stmt.close();
+   					dbConn.close();
+   				} catch (SQLException e) {
+   					// TODO Auto-generated catch block
+   					e.printStackTrace();
+   				}
+               }
+           }
+
+		return  largest = (int) Collections.max(num);
 
 
 
 
-		public void displayTransactionHistory() {
-			Database database = new Database();
-	        Connection dbConn = database.getConnection();
+       }
 
-			try {
-				String sql = "select * from Accounts Where USERNAME =?";
-				PreparedStatement pst = dbConn.prepareStatement(sql);
-				pst.setString(1,Input.username);
-				rs=pst.executeQuery();
-				JTable table = new JTable(buildTableModel(rs));
-				JOptionPane.showMessageDialog(null, new JScrollPane(table));
+       public int getAccountNumSavings(String user) {
+    	   ArrayList num = new ArrayList();
 
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+    	   Database database = new Database();
+           Connection dbConn = database.getConnection();
+           Statement stmt = null;
 
-		}
+           int largest;
+
+
+
+           try {
+               String sql = "SELECT ID FROM ACCOUNTS WHERE USERNAME = ? AND ACCOUNTTYPE = ?";
+               PreparedStatement pst = dbConn.prepareStatement(sql);
+               pst.setString(1, user);
+               pst.setString(1, "Savings");
+               ResultSet rs = pst.executeQuery();
+
+               while (rs.next()){
+               	num.add(rs.getInt("ID"));
+               }
+
+
+           } catch (SQLException e) {
+               System.out.println("------------------TableInsert-----------------");
+               System.out.println("Cannot insert into table: " + e);
+               System.out.println("--------------------------------------------------------");
+           } finally {
+               if (stmt != null) {
+                   try {
+   					stmt.close();
+   					dbConn.close();
+   				} catch (SQLException e) {
+   					// TODO Auto-generated catch block
+   					e.printStackTrace();
+   				}
+               }
+           }
+
+		return  largest = (int) Collections.max(num);
+
+
+
+
+       }
 
 		public static DefaultTableModel buildTableModel(ResultSet rs)
 		        throws SQLException {
@@ -276,5 +354,7 @@ public class Display extends JDialog {
 	        */
 	    }
 	}
+
+
 
 
