6e2d49c515e13a753ce897fa30f24fe6eebea09e
michaelodio <michaelfodio@gmail.com>
2018-10-01 15:29:42 -0400

    added withdraw method functionality

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,11 +1,9 @@
+package proto2;
+
 import java.sql.SQLException;
 
 public class Transaction {
 
-
-    /*public Transaction(Account account){
-        this.account = account;
-    }*/
     public static void depositFunds(double amount, int actNum){
         try {
         	double prevBal = 0;
@@ -13,7 +11,7 @@ public class Transaction {
 
         	prevBal = Input.getBalance(actNum);
         	newBal = prevBal + amount;
-             //updates db
+
 			Input.setBalance(newBal, actNum);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -22,14 +20,30 @@ public class Transaction {
 
 
     }
-    /*public withdrawFunds(double amount){
-        if (account.getBalance() >= amount){
-            account.setBalance(account.getBalance() - amount);
-            //TODO add transaction to database;
-        }else{
-            throw new InsufficientFundsException();
-        }
-    }*/
+    public static void withdrawFunds(double amount,int actNum){
+
+    	 try {
+         	double prevBal = 0;
+         	double newBal = 0;
+
+         	prevBal = Input.getBalance(actNum);
+
+         	if(prevBal>= amount) {
+         		newBal = prevBal - amount;
+         		Input.setBalance(newBal, actNum);
+         	}
+
+
+
+ 		} catch (SQLException e) {
+ 			// TODO Auto-generated catch block
+ 			e.printStackTrace();
+ 		}
+
+
+
+
+    }
     /*public calculateInterest(Account account){
         //TODO send account to Interest to calculate interest;
         //TODO add transaction to database;
