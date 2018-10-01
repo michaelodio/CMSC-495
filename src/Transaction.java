c7334a97d859d7dd2d7a5d6d596dea979d944211
michaelodio <michaelfodio@gmail.com>
2018-10-01 15:11:42 -0400

    modified deposit funds method

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,19 +1,31 @@
-public class Transaction {
-    Account account;
+import java.sql.SQLException;
 
-    public Transaction(Account account){
+public class Transaction {
+
+
+    /*public Transaction(Account account){
         this.account = account;
-    }
-    public double depositFunds( double amount){
-        //account.setBalance(account.getBalance() + amount);
-        //TODO add transaction to database;
-        return 0d;
+    }*/
+    public static void depositFunds(double amount, int actNum){
+        try {
+        	double prevBal = 0;
+        	double newBal = 0;
+
+        	prevBal = Input.getBalance(actNum);
+        	newBal = prevBal + amount;
+             //updates db
+			Input.setBalance(newBal, actNum);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
     }
     /*public withdrawFunds(double amount){
         if (account.getBalance() >= amount){
             account.setBalance(account.getBalance() - amount);
             //TODO add transaction to database;
-
         }else{
             throw new InsufficientFundsException();
         }
